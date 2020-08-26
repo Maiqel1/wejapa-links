404062e399bead3df10e04a08faaf9116811a3ce
Maiqel1 <66220194+Maiqel1@users.noreply.github.com>
2020-08-27 00:25:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-let result = [2];
+var form = document.getElementById('form');
+var itemList = document.getElementById('items');
 
-function fizzBuzz(){
-    return result;
+form.addEventListener('submit', addItem);
+
+function addItem(e){
+    e.preventDefault();
+
+    var newItem = document.getElementById('link').value;
+
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    console.log(li);
+
+    li.appendChild(document.createTextNode(newItem));
+
+    var copy = document.createElement('button');
+
+    copy.className = 'btn btn-md float-right '
+    copy.style.marginLeft = '200px'
+    copy.appendChild(document.createTextNode('copy'));
+
+    copy.addEventListener('click', change);
+    function change(){
+        
+        copy.textContent = 'copied!';
+        copy.style.backgroundColor = ' hsl(257, 27%, 26%)';
+    }
+
+    li.appendChild(copy);
+
+    itemList.appendChild(li);
 }
 
-console.log(fizzBuzz());
